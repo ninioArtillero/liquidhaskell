fed97fad519df8c950af416674b4853e6dbf407b
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-11-11 17:29:37 +0000

    Account for changes in GHC to simplifyInfer, EpaComments, and isEqPred

--- liquidhaskell-boot/src/Language/Haskell/Liquid/GHC/Plugin/Types.hs
+++ liquidhaskell-boot/src/Language/Haskell/Liquid/GHC/Plugin/Types.hs
@@ -31,7 +31,6 @@ module Language.Haskell.Liquid.GHC.Plugin.Types
 
 import           Data.Binary                             as B
 import           Data.Data                               ( Data )
-import           Data.Foldable
 import           GHC.Generics                      hiding ( moduleName )
 
 import qualified Data.HashSet        as HS
