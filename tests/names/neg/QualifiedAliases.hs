cbea8706d184e0bcc82893eb9131d90133850c86
Xavier Góngora <ixbalanque@protonmail.ch>
2025-07-03 17:30:57 -0600

    update test

--- tests/names/neg/QualifiedAliases.hs
+++ tests/names/neg/QualifiedAliases.hs
@@ -5,16 +5,19 @@
 -- the same name @INat@. With current behavior this module produces a
 -- _Multiple definition of Type Alias_ error when using the alias unqualified,
 -- and an _Unknown type constructor_ error with the qualified alias.
--- NOTE: This test will be moved to @names-pos@ after fixing issue #2841
+-- TEMP-NOTE: This test should fail after fixing issue #2841 and moved to @names-pos@.
 module QualifiedAliases where
 
+import Data.Int (Int32)
 import qualified Nat1 as N
 import Nat2
 
 {-@ llength :: [a] -> INat @-}
+llength :: [a] -> Int32
 llength [] = 0
-llength (x : xs) = 1 + length xs
+llength (x : xs) = 1 + llength xs
 
 {-@ llength' :: [a] -> N.INat @-}
+llength' :: [a] -> Int
 llength' [] = 0
-llength' (x : xs) = 1 + length xs
+llength' (x : xs) = 1 + llength' xs
